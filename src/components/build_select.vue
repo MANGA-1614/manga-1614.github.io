c946d960054820c90ae5bfc803f27af86a9ce28e
MANGA-1614 <johncbao1@gmail.com>
2021-08-28 16:48:32 -0400

    Placed links in span so that users can hover

--- src/components/build_select.vue
+++ src/components/build_select.vue
@@ -3,11 +3,17 @@
         <button @click="toggle()" id="btn_build_list">Select Build List</button>
 
         <div id="build_content" v-show=this.isHidden>
-            <router-link to="/build/No_Orbs_No_DLC"> No Omnipotent Orb, No DLC</router-link>
+            <router-link to="/build/No_Orbs_No_DLC">
+                <span class="build_link">No Omnipotent Orb, No DLC</span>
+            </router-link>
             <br>
-            <router-link to="/build/Orbs_No_DLC">With Omnipotent Orb, No DLC</router-link>
+            <router-link to="/build/Orbs_No_DLC">
+                <span class="build_link">With Omnipotent Orb, No DLC</span>
+            </router-link>
             <br>
-            <router-link to="/build/Orbs_DLC">With Omnipotent Orb, With DLC</router-link>
+            <router-link to="/build/Orbs_DLC">
+                <span class="build_link">With Omnipotent Orb, With DLC</span>
+            </router-link>
         </div>
     </div>
 </template>
